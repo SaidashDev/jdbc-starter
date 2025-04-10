d7fa22bfe7c8efbab2f054c5ca1e6303ee435446
Saidash <SaidashKGEU@yandex.ru>
2025-04-11 01:27:59 +0300

    005 Statement. DDL операции

--- src/com/saidash/jdbc/starter/JdbcRunner.java
+++ src/com/saidash/jdbc/starter/JdbcRunner.java
@@ -6,13 +6,25 @@ import org.postgresql.Driver;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class JdbcRunner {
     public static void main(String[] args) throws SQLException {
 
-        try (var connection = ConnectionManager.open()) {
+        String sql = """
+                CREATE TABLE IF NOT EXISTS info (
+                    id SERIAL PRIMARY KEY,
+                    data TEXT NOT NULL 
+                );
+                """;
 
+        try (var connection = ConnectionManager.open();
+             var statement = connection.createStatement()) {
             System.out.println(connection.getTransactionIsolation());
+            System.out.println(connection.getSchema());
+            var executeResult = statement.execute(sql);
+            System.out.println(executeResult);
+
         }
     }
 }
